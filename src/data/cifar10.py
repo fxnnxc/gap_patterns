6a80afa30f0924702ac5739294385f69c1fdabb3
fxnnxc <bumjin@kaist.ac.kr>
2022-12-20 03:13:44 +0000

    update mean and std to imagenet

--- src/data/cifar10.py
+++ src/data/cifar10.py
@@ -2,10 +2,12 @@ import torchvision
 import torchvision.datasets as datasets
 import torchvision.transforms as T
 
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225] 
 def get_cifar10(root, size):
     transform = T.Compose([T.Resize((size, size)),
                                   T.ToTensor(),
-                                  T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                                  T.Normalize(mean, std)
                                  ])
     train_ds = datasets.CIFAR10(root = root,  
                                     train = True,  
